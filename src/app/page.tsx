da28ff625b3b8ffce99ffff2a615847b4ad8ee55
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-10-29 23:43:02 -0300

    create and render hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <Hero />
+    </main>
+  );
 }
