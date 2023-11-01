cf67071e1d8254a10eb25d9824a18ec827626519
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-11-01 10:16:38 -0300

    create query to fetch data

--- src/components/Newest.tsx
+++ src/components/Newest.tsx
@@ -1,3 +1,23 @@
-export default function Newest() {
+import { simplifiedProduct } from "@/interface";
+import { client } from "@/lib/sanity";
+
+async function getData() {
+  const query = `*[_type == "product][0...3] | order(_createdAt desc){
+    _id, 
+    price,
+    name,
+    "slug": slug.current,
+    "categoryName": category->name,
+    "imageUrl": iamges[0].asset->url
+  }`;
+
+  const data = client.fetch(query);
+
+  return data;
+}
+
+export default async function Newest() {
+  const data: simplifiedProduct[] = await getData();
+
   return <div></div>;
 }
