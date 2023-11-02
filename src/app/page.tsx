49d178ca35028a81cec14c752e11ed97dca8a21c
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-11-02 09:42:47 -0300

    render newest component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Newest from "@/components/Newest";
 
 export default function Home() {
   return (
     <main className="bg-white pb-6 sm:pb-8 lg:pb-12">
       <Hero />
+      <Newest />
     </main>
   );
 }
