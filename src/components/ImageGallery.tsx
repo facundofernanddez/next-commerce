a332632cd855136a79bc9374ff728b946e226e3e
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-11-04 08:46:10 -0300

    image gallery done

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,12 +1,20 @@
+"use client";
+
 import { urlFor } from "@/lib/sanity";
-import { Divide } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface iAppProps {
   images: any;
 }
 
 export default function ImageGallery({ images }: iAppProps) {
+  const [bigImage, setBigImage] = useState(images[0]);
+
+  const handleSmallImageClick = (image: any) => {
+    setBigImage(image);
+  };
+
   return (
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
@@ -21,9 +29,24 @@ export default function ImageGallery({ images }: iAppProps) {
               width={200}
               alt="photo"
               className="w-full h-full object-cover object-center cursor-pointer"
+              onClick={() => handleSmallImageClick(image)}
             />
           </div>
         ))}
+      </div>
+
+      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+        <Image
+          src={urlFor(bigImage).url()}
+          alt="photo"
+          width={500}
+          height={500}
+          className="h-full w-full object-cover object-center"
+        />
+
+        <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">
+          Sale
+        </span>
       </div>
     </div>
   );
